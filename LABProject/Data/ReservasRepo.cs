67ce337a6bbe7478685c23a525896760b0be8aef
steven mata <smata9903@gmail.com>
2025-11-24 06:36:10 -0600

    Se crea el repositorio Data

---
+++ LABProject/Data/ReservasRepo.cs
@@ -0,0 +1,25 @@
+﻿using LABproject.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LABproject.Data
+{
+    public static class ReservasRepo
+    {
+        private static readonly List<Reserva> _reservas = new();
+        private static int _nextId = 1;
+
+        public static IReadOnlyList<Reserva> ObtenerTodos() => _reservas.AsReadOnly();
+
+        public static void Agregar(Reserva r)
+        {
+            r.Id = _nextId++;
+            _reservas.Add(r);
+        }
+
+        public static bool ExisteCodigo(string codigo)
+        {
+            return _reservas.Any(x => x.Codigo == codigo);
+        }
+    }
+}
