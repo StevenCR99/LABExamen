c9bae2f468da65045633ba2f6854678e39165768
steven mata <smata9903@gmail.com>
2025-11-24 06:36:23 -0600

    Se crea el controlador

---
+++ LABProject/Controllers/ReservasController.cs
@@ -0,0 +1,40 @@
+﻿using LABproject.Data;
+using LABproject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace labproject.Controllers
+{
+    public class ReservasController : Controller
+    {
+        public IActionResult Index()
+        {
+            var reservas = ReservasRepo.ObtenerTodos();
+            return View(reservas);
+        }
+
+        public IActionResult Create()
+        {
+            return View(new Reserva());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Reserva model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            if (ReservasRepo.ExisteCodigo(model.Codigo))
+            {
+                ModelState.AddModelError(nameof(model.Codigo), $"El código '{model.Codigo}' ya existe.");
+                return View(model);
+            }
+
+            ReservasRepo.Agregar(model);
+            TempData["Mensaje"] = $"Reserva '{model.Codigo}' registrada correctamente.";
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
